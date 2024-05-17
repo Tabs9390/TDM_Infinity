ff0e35ecefc50a8fcc10b031b9c20d0ea275ccb3
Tabs9390 <127233992+Tabs9390@users.noreply.github.com>
2024-05-17 14:35:31 +0200

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,5 +1,5 @@
 import { DisplayValueHeader } from 'pixel_combats/basic';
-import { Game, Players, Inventory, LeaderBoard, BuildBlocksSet, Teams, Damage, BreackGraph, Ui, Properties, GameMode, Spawns, Timers, TeamsBalancer, NewGame, NewGameVote } from 'pixel_combats/room';
+import { Game, Players, Inventory, LeaderBoard, BuildBlocksSet, Teams, Damage, BreackGraph, Ui, Properties, GameMode, Spawns, Timers, TeamsBalancer, NewGame, NewGameVote, Player } from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
 // настройки
